373668a2cf67674b6e1fe65f9a7383f9db64ac22
Daiki Matoba <telnetstat@gmail.com>
2018-02-05 21:39:58 +0900

    Add response tests

--- spec/line/bot/rich_menu_spec.rb
+++ spec/line/bot/rich_menu_spec.rb
@@ -36,6 +36,8 @@ RICH_MENU_LIST_CONTENT = <<"EOS"
 }
 EOS
 
+RICH_MENU_IMAGE_FILE_PATH = 'spec/fixtures/line/bot/rich_menu_01.png'
+
 WebMock.allow_net_connect!
 
 describe Line::Bot::Client do
@@ -50,23 +52,25 @@ describe Line::Bot::Client do
 
   it 'gets a list of rich menus' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/list'
-    stub_request(:get, uri_template).to_return { |request| { :body => RICH_MENU_LIST_CONTENT, :status => 200 } }
+    stub_request(:get, uri_template).to_return(:body => RICH_MENU_LIST_CONTENT, :status => 200)
 
-    client.get_rich_menus
+    response = client.get_rich_menus
     expect(WebMock).to have_requested(:get, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/list')
+    expect(response.body).to eq RICH_MENU_LIST_CONTENT
   end
 
   it 'gets a rich menu' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567'
-    stub_request(:get, uri_template).to_return { |request| { :body => RICH_MENU_CONTENT, :status => 200 } }
+    stub_request(:get, uri_template).to_return(:body => RICH_MENU_CONTENT, :status => 200)
 
-    client.get_rich_menu('1234567')
+    response = client.get_rich_menu('1234567')
     expect(WebMock).to have_requested(:get, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567')
+    expect(response.body).to eq RICH_MENU_CONTENT
   end
 
   it 'creates a rich menu' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu'
-    stub_request(:post, uri_template).to_return { |request| { :body => RICH_MENU_CONTENT, :status => 200 } }
+    stub_request(:post, uri_template).to_return(:body => RICH_MENU_CONTENT, :status => 200)
 
     client.create_rich_menu(JSON.parse(RICH_MENU_CONTENT))
     expect(WebMock).to have_requested(:post, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu')
@@ -75,7 +79,7 @@ describe Line::Bot::Client do
 
   it 'deletes a rich menu' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567'
-    stub_request(:delete, uri_template).to_return { |request| { :body => '{}', :status => 200 } }
+    stub_request(:delete, uri_template).to_return(:body => '{}', :status => 200)
 
     client.delete_rich_menu('1234567')
     expect(WebMock).to have_requested(:delete, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567')
@@ -83,15 +87,16 @@ describe Line::Bot::Client do
 
   it 'gets rich menu linked to a user' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/user/1234567/richmenu'
-    stub_request(:get, uri_template).to_return { |request| { :body => '{"richMenuId":"7654321"}', :status => 200 } }
+    stub_request(:get, uri_template).to_return(:body => '{"richMenuId":"7654321"}', :status => 200)
 
-    client.get_user_rich_menu('1234567')
+    response = client.get_user_rich_menu('1234567')
     expect(WebMock).to have_requested(:get, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/user/1234567/richmenu')
+    expect(response.body).to eq '{"richMenuId":"7654321"}'
   end
 
   it 'links a rich menu to a user' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/user/1234567/richmenu/7654321'
-    stub_request(:post, uri_template).to_return { |request| { :body => '{}', :status => 200 } }
+    stub_request(:post, uri_template).to_return(:body => '{}', :status => 200)
 
     client.link_user_rich_menu('1234567', '7654321')
     expect(WebMock).to have_requested(:post, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/user/1234567/richmenu/7654321')
@@ -99,7 +104,7 @@ describe Line::Bot::Client do
 
   it 'unlinks a rich menu from a user' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/user/1234567/richmenu'
-    stub_request(:delete, uri_template).to_return { |request| { :body => '{}', :status => 200 } }
+    stub_request(:delete, uri_template).to_return(:body => '{}', :status => 200)
 
     client.unlink_user_rich_menu('1234567')
     expect(WebMock).to have_requested(:delete, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/user/1234567/richmenu')
@@ -107,20 +112,19 @@ describe Line::Bot::Client do
 
   it 'gets an image associated with a rich menu' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567/content'
-    stub_request(:get, uri_template).to_return { |request| { :body => '', :status => 200 } }
+    stub_request(:get, uri_template).to_return(:body => open(RICH_MENU_IMAGE_FILE_PATH).read, :status => 200)
 
-    client.get_rich_menu_image('1234567')
+    response = client.get_rich_menu_image('1234567')
     expect(WebMock).to have_requested(:get, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567/content')
+    expect(response.body).to eq open(RICH_MENU_IMAGE_FILE_PATH).read
   end
 
   it 'uploads and attaches an image to a rich menu' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567/content'
-    stub_request(:post, uri_template).to_return { |request| { :body => '{}', :status => 200 } }
+    stub_request(:post, uri_template).to_return(:body => '{}', :status => 200)
 
-    file_path = 'spec/fixtures/line/bot/rich_menu_01.png'
-
-    client.create_rich_menu_image('1234567', open(file_path))
+    client.create_rich_menu_image('1234567', open(RICH_MENU_IMAGE_FILE_PATH))
     expect(WebMock).to have_requested(:post, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/richmenu/1234567/content')
-                         .with(:body => open(file_path).read)
+                         .with(:body => open(RICH_MENU_IMAGE_FILE_PATH).read)
   end
 end
