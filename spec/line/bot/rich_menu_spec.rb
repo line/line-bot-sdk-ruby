7cb6ff552e8882c8d805bfbc599d91d7113df50b
toduq <toduqq@gmail.com>
2019-05-27 21:16:16 +0900

    Fix invalid payload of RichMenu testcase

--- spec/line/bot/rich_menu_spec.rb
+++ spec/line/bot/rich_menu_spec.rb
@@ -5,12 +5,13 @@ require 'tempfile'
 
 RICH_MENU_CONTENT = <<"EOS"
 {
-  "richMenuId": "1234567",
   "size": {
      "width": 2500,
      "height": 1686
    },
    "selected": false,
+   "name": "Nice richmenu",
+   "chatBarText": "Tap here",
    "areas": [
      {
        "bounds": {
