e4cf7eeafd3df2dab76c28b324626af915560120
onikunai <greentea5461@gmail.com>
2020-02-18 21:41:03 +0900

    test

--- app/return.rb
+++ app/return.rb
@@ -1,17 +1,17 @@
 require 'sinatra'
 require 'line/bot'
 
-# 微小変更部分！確認用。
-get '/' do
-  "Hello world"
-end
+# # 微小変更部分！確認用。
+# get '/' do
+#   "Hello world"
+# end
 
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
-end
+# def client
+#   @client ||= Line::Bot::Client.new { |config|
+#     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+#     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+#   }
+# end
 
 post '/callback' do
   body = request.body.read
--- config/config.ru
+++ config/config.ru
@@ -1,3 +1,15 @@
+# 微小変更部分！確認用。
+get '/' do
+  "Hello world"
+end
+
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
 require './app/return'
 # require './app_main'
 run Sinatra::Application
