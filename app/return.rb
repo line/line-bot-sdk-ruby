495448d32381e277cb492e59dfa1b53cf7ec6106
onikunai <greentea5461@gmail.com>
2020-02-20 02:21:28 +0900

    Update return.rb

--- app/return.rb
+++ app/return.rb
@@ -11,10 +11,10 @@ end
 post '/callback' do
   body = request.body.read
 
-  signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
-  end
+  # signature = request.env['HTTP_X_LINE_SIGNATURE']
+  # unless client.validate_signature(body, signature)
+  #   error 400 do 'Bad Request' end
+  # end
 
   events = client.parse_events_from(body)
   events.each { |event|
