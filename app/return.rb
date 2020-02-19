9bf4c338287a88ebc0a4d9756dd5f91744bd7147
onikunai <greentea5461@gmail.com>
2020-02-20 01:45:28 +0900

    test

--- app/return.rb
+++ app/return.rb
@@ -1,35 +1,47 @@
-# require 'sinatra'
-# require 'line/bot'
+require 'sinatra'
+require 'line/bot'
 
-# post '/callback' do
-#   body = request.body.read
-#   events = client.parse_events_from(body)
-#   events.each { |event|
-#     loop{
-#       if event.message['text'] == 'また明日' then
-#         break
-#       else
-#         message = {
-#           type: 'text',
-#           text: event.message['text']
-#         }
-#         client.reply_message(event['replyToken'], message)
-#       end
-#     }
-#   }
-# end
+post '/callback' do
+  body = request.body.read
 
-loop{
-  if event.message['text'] == 'また明日' then
-    break
-  else
-    message = {
-      type: 'text',
-      text: event.message['text']
-    }
-    client.reply_message(event['replyToken'], message)
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
   end
-}
+
+  events = client.parse_events_from(body)
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+        response = client.get_message_content(event.message['id'])
+        tf = Tempfile.open("content")
+        tf.write(response.body)
+      end
+    end
+  }
+
+  "OK"
+end
+
+# loop{
+#   if event.message['text'] == 'また明日' then
+#     break
+#   else
+#     message = {
+#       type: 'text',
+#       text: event.message['text']
+#     }
+#     client.reply_message(event['replyToken'], message)
+#   end
+# }
 
 # post '/callback' do
 #   body = request.body.read
