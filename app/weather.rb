fa7ce1f2f1c364cf4906b7f298a5da04bb256287
onikunai <greentea5461@gmail.com>
2020-02-19 21:07:44 +0900

    Update weather.rb

--- app/weather.rb
+++ app/weather.rb
@@ -17,7 +17,7 @@ class Say_weather
   def message
     message = {
       type: 'text',
-      text: "あはは\nと送信してください。"
+      text: "今日、#{info.today()} 東京の天気です。\n 天気    ：#{info.todayTelop()}\n 最高気温：#{info.todayTempMax()}\n 最低気温：#{info.todayTempMin()}\n\n明日、#{info.tmrw()} 東京の天気です。\n 天気    ：#{info.tmrwTelop()}\n 最高気温：#{info.tmrwTempMax()}\n 最低気温：#{info.tmrwTempMin()}\n【概要】\n #{info.description()}"
     }
   end
 end
