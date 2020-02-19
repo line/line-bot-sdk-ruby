4a96ed6aed2de4cfdcafa426ccef80c231fd5641
onikunai <greentea5461@gmail.com>
2020-02-20 01:50:42 +0900

    Update return.rb

--- app/return.rb
+++ app/return.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'line/bot'
 
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
 post '/callback' do
   body = request.body.read
 
