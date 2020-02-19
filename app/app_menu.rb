92eadc023b19b78ff3c12c4a095e89dba5dbceaf
onikunai <greentea5461@gmail.com>
2020-02-19 22:15:22 +0900

    Update app_menu.rb

--- app/app_menu.rb
+++ app/app_menu.rb
@@ -36,7 +36,7 @@ post '/callback' do
       when Line::Bot::Event::MessageType::Text
         if event.message['text'] == '天気' then
           require './app/weather'
-          # Say_weather = Say_weather.new
+          Say_weather = Say_weather.new
           message = Say_weather.message
           client.reply_message(event['replyToken'], message)
         elsif event.message['text'] == 'おうむ返し' then
