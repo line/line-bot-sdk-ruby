90a36a2819f76173539924b8d1b04245fca0e8f4
onikunai <greentea5461@gmail.com>
2020-02-19 18:47:23 +0900

    menu

--- app/app_menu.rb
+++ app/app_menu.rb
@@ -15,3 +15,36 @@ end
 
 # require './app/return'
 require './app/weather'
+
+post '/callback' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        if event.message['text'] == '天気' then
+          require './app/weather'
+
+        elsif event.message['text'] == 'おうむ返し' then
+          require './app/return'
+        
+        else
+          message = {
+            type: 'text',
+            text: "「天気」か「おうむ返し」\nとメッセージを送信して下さい。"
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+
+  }
+
+  "OK"
+end
