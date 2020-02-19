4a860427db14b7097c3deb2429bd45c170a99e61
onikunai <greentea5461@gmail.com>
2020-02-19 18:31:26 +0900

    Update weather.rb

--- app/weather.rb
+++ app/weather.rb
@@ -40,8 +40,7 @@ post '/callback' do
           message = {
             type: 'text',
             text: 
-              "今日、#{info.today()} 東京の天気です。\n 天気    ：#{info.todayTelop()}\n 最高気温：#{info.todayTempMax()}\n 最低気温：#{info.todayTempMin()}
-              
+              "今日、#{info.today()} 東京の天気です。\n 天気    ：#{info.todayTelop()}\n 最高気温：#{info.todayTempMax()}\n 最低気温：#{info.todayTempMin()}\n
               明日、#{info.tmrw()} 東京の天気です。
               天気    ：#{info.tmrwTelop()}
               最高気温：#{info.tmrwTempMax()}
