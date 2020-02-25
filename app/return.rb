35c87521675f0455cffada770dec96c942c81f83
onikunai <greentea5461@gmail.com>
2020-02-25 16:30:06 +0900

    fin

--- app/return.rb
+++ app/return.rb
@@ -17,17 +17,17 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
-        # message = {
-        #   type: 'text',
-        #   # text: event.message['text']
-        #   text: '暑い'
-        # }
-        # client.reply_message(event['replyToken'], message)
         message = {
           type: 'text',
-          text: "暑いよ"
+          text: event.message['text']
+          # text: '暑い'
         }
         client.reply_message(event['replyToken'], message)
+        # message = {
+        #   type: 'text',
+        #   text: "暑いよ"
+        # }
+        # client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
