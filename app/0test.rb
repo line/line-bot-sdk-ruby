3431cb7d5bde02cea64a6f6374cb4dce97d5cd63
onikunai <greentea5461@gmail.com>
2020-02-26 02:20:21 +0900

    Update 0test.rb

--- app/0test.rb
+++ app/0test.rb
@@ -28,7 +28,7 @@ post '/callback' do
         message = {
           type: 'text',
           # text: event.message['text'] + num
-          text: num.to_s
+          text: "テスト + #{num.to_s} +回目"
         }
         # 必ずおはようを返す。
         # message = {
