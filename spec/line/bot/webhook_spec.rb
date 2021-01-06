1898e9a696db75f23ae52d268f4419a526c11b52
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2021-01-06 16:31:35 +0900

    add set webhook endpoint test

--- spec/line/bot/webhook_spec.rb
+++ spec/line/bot/webhook_spec.rb
@@ -9,6 +9,13 @@ WEBHOOK_ENDPOINT_CONTENT = <<"EOS"
 }
 EOS
 
+UPDATE_WEBHOOK_ENDPOINT = 'https://example.com/hoge'
+UPDATE_WEBHOOK_ENDPOINT_CONTENT = <<"EOS"
+{
+  "endpoint": "#{UPDATE_WEBHOOK_ENDPOINT}"
+}
+EOS
+
 describe Line::Bot::Client do
   let(:client) do
     dummy_config = {
@@ -30,5 +37,14 @@ describe Line::Bot::Client do
       endpoint: 'https://example.com/test',
       active: true
     )
+  end
+
+  it 'set webhook endpoint' do
+    uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/channel/webhook/endpoint'
+    stub_request(:put, uri_template).to_return(body: '{}', status: 200)
+
+    client.set_webhook_endpoint(UPDATE_WEBHOOK_ENDPOINT)
+    expect(WebMock).to have_requested(:put, Line::Bot::API::DEFAULT_ENDPOINT + '/bot/channel/webhook/endpoint')
+      .with(body: JSON.parse(UPDATE_WEBHOOK_ENDPOINT_CONTENT).to_json)
   end
 end
