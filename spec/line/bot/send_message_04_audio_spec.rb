36b74f6903369d82c0649cd0470593e9842e65c7
Yoshiyuki Kinjo <yskkin@gmail.com>
2021-11-07 14:14:09 +0900

    push_message and multicast can set additional payload

--- spec/line/bot/send_message_04_audio_spec.rb
+++ spec/line/bot/send_message_04_audio_spec.rb
@@ -28,6 +28,32 @@ describe Line::Bot::Client do
     expect(response.body).to eq(expected)
   end
 
+  it 'pushes the audio message with additional payload' do
+    uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/message/push'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = Line::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    user_id = 'user_id'
+    message = {
+      'type' => 'audio',
+      'originalContentUrl' => 'https://example.com/audio.mp3',
+      'duration' => 120000
+    }
+    response = client.push_message(user_id, message, payload: {'customAggregationUnits' => ['test']})
+
+    expected = {
+      'to' => user_id,
+      'messages' => [
+        message
+      ],
+      'customAggregationUnits' => ['test']
+    }
+    expect(JSON.parse(response.body)).to eq(expected)
+  end
+
   it 'replies the audio message' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/message/reply'
     stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
@@ -76,5 +102,31 @@ describe Line::Bot::Client do
       ]
     }.to_json
     expect(response.body).to eq(expected)
+  end
+
+  it 'multicasts the audio message with additional payload' do
+    uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/message/multicast'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = Line::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    user_ids = ['user1', 'user2']
+    message = {
+      'type' => 'audio',
+      'originalContentUrl' => 'https://example.com/audio.mp3',
+      'duration' => 120000
+    }
+    response = client.multicast(user_ids, message, payload: {'customAggregationUnits' => ['test']})
+
+    expected = {
+      'to' => user_ids,
+      'messages' => [
+        message
+      ],
+      'customAggregationUnits' => ['test']
+    }
+    expect(JSON.parse(response.body)).to eq(expected)
   end
 end
--- spec/line/bot/send_message_05_location_spec.rb
+++ spec/line/bot/send_message_05_location_spec.rb
@@ -30,6 +30,34 @@ describe Line::Bot::Client do
     expect(response.body).to eq(expected)
   end
 
+  it 'pushes the location message with additional payload' do
+    uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/message/push'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = Line::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    user_id = 'user_id'
+    message = {
+      'type' => 'location',
+      'title' => 'LINE Corporation.',
+      'address' => 'Hikarie  Shibuya-ku Tokyo 151-0002',
+      'latitude' => 35.61823286112982,
+      'longitude' => 139.72824096679688,
+    }
+    response = client.push_message(user_id, message, payload: {'customAggregationUnits' => ['test']})
+
+    expected = {
+      'to' => user_id,
+      'messages' => [
+        message
+      ],
+      'customAggregationUnits' => ['test']
+    }
+    expect(JSON.parse(response.body)).to eq(expected)
+  end
+
   it 'replies the location message' do
     uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/message/reply'
     stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
@@ -82,5 +110,33 @@ describe Line::Bot::Client do
       ]
     }.to_json
     expect(response.body).to eq(expected)
+  end
+
+  it 'multicasts the location message with additional payload' do
+    uri_template = Addressable::Template.new Line::Bot::API::DEFAULT_ENDPOINT + '/bot/message/multicast'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = Line::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    user_ids = ['user1', 'user2']
+    message = {
+      'type' => 'location',
+      'title' => 'LINE Corporation.',
+      'address' => 'Hikarie  Shibuya-ku Tokyo 151-0002',
+      'latitude' => 35.61823286112982,
+      'longitude' => 139.72824096679688,
+    }
+    response = client.multicast(user_ids, message, payload: {'customAggregationUnits' => ['test']})
+
+    expected = {
+      'to' => user_ids,
+      'messages' => [
+        message
+      ],
+      'customAggregationUnits' => ['test']
+    }
+    expect(JSON.parse(response.body)).to eq(expected)
   end
 end
