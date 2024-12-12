c84e669d80952321e57048336eb0fb6677832899
Motoi Okuzono <motoi.okuzono+github@gmail.com>
2024-12-12 11:44:06 +0900

    Run rubocop -A

--- lib/line/bot/v2/reserved_words.rb
+++ lib/line/bot/v2/reserved_words.rb
@@ -3,7 +3,8 @@ module Line
     module V2
       # CodeGen add _ prefix to reserved words
       # see: https://github.com/OpenAPITools/openapi-generator/blob/master/modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractRubyCodegen.java
-      RESERVED_WORDS = %i[__FILE__ and def end in or self unless __LINE__ begin defined? ensure module redo super until BEGIN break do false next rescue then when END case else for nil retry true while alias class elsif if not return undef yield]
+      RESERVED_WORDS = %i[__FILE__ and def end in or self unless __LINE__ begin defined? ensure module redo super until BEGIN break do false next rescue then when END case else for nil retry true
+                          while alias class elsif if not return undef yield]
     end
   end
 end
