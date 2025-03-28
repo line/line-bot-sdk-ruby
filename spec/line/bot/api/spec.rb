fe521a66561af8462ff8a313a7b34a525f71453c
Yuta Kasai <yuta.kasai@linecorp.com>
2025-03-28 18:31:14 +0900

    NO-ISSUE test

--- spec/line/bot/api/spec.rb
+++ spec/line/bot/api/spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Line::Bot::API do
   it 'has a version number' do
     expect(Line::Bot::API::VERSION).not_to be nil
-    expect(1).to eq(2)
+    expect(1).to be 2
   end
 end
