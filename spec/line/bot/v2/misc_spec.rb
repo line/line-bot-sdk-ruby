9a8ed369741219744833e0d810f161ad0efdf2da
Yuta Kasai <yuta.kasai@linecorp.com>
2025-04-04 15:46:35 +0900

    NO-ISSUE Add test for stateless channel access token

--- spec/line/bot/v2/misc_spec.rb
+++ spec/line/bot/v2/misc_spec.rb
@@ -247,6 +247,42 @@ describe 'misc' do
 
       expect(status_code).to eq(200)
       expect(body.access_token).to eq('test-access-token')
+
+      expect(WebMock).to(have_requested(:post, "https://api.line.me/v2/oauth/accessToken")
+                            .with { |req| !req.headers.key?("Authorization") })
+    end
+  end
+
+  describe 'POST /oauth2/v3/token' do
+    let(:client) { Line::Bot::V2::ChannelAccessToken::ApiClient.new }
+    let(:grant_type) { 'client_credentials' }
+    let(:client_id) { 'test-client-id' }
+    let(:client_secret) { 'test-client-secret' }
+    let(:response_body) { { "access_token" => "test-access-token", "expires_in" => 2592000, "token_type" => "Bearer" }.to_json }
+    let(:response_code) { 200 }
+
+    it "when the two parameters are nil, they will not be sent" do
+      stub_request(:post, "https://api.line.me/oauth2/v3/token")
+        .with(
+          body: { client_id: "test-client-id", client_secret: "test-client-secret", grant_type: "client_credentials" },
+          headers: {
+            'Content-Type' => 'application/x-www-form-urlencoded'
+          }
+        )
+        .to_return(status: response_code, body: response_body, headers: { 'Content-Type' => 'application/json' })
+
+      body, status_code, headers = client.issue_stateless_channel_token_with_http_info(
+        grant_type: grant_type,
+        client_id: client_id,
+        client_secret: client_secret
+      )
+
+      expect(status_code).to eq(200)
+      expect(body.access_token).to eq('test-access-token')
+      expect(body.token_type).to eq('Bearer')
+
+      expect(WebMock).to(have_requested(:post, "https://api.line.me/oauth2/v3/token")
+                           .with { |req| !req.headers.key?("Authorization") })
     end
   end
 
@@ -497,7 +533,7 @@ describe 'misc' do
                            .with { |req| !req.headers.key?("X-Line-Retry-Key") })
     end
 
-    it 'request with  x-line-retry-key header - success' do
+    it 'request with x-line-retry-key header - success' do
       retry_key = 'f03c3eb4-0267-4080-9e65-fffa184e1933'
       stub_request(:post, "https://api.line.me/v2/bot/message/push")
         .with(
