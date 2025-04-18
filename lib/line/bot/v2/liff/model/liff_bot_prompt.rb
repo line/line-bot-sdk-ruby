60078393902788157f6d25e25d3cdb066df11145
Yuta Kasai <yuta.kasai@linecorp.com>
2025-04-19 01:17:32 +0900

    NO-ISSUE gen

--- lib/line/bot/v2/liff/model/liff_bot_prompt.rb
+++ lib/line/bot/v2/liff/model/liff_bot_prompt.rb
@@ -13,43 +13,7 @@ module Line
       module Liff
         # Specify the setting for bot link feature with one of the following values:  `normal`: Display the option to add the LINE Official Account as a friend in the channel consent screen. `aggressive`: Display a screen with the option to add the LINE Official Account as a friend after the channel consent screen. `none`: Don't display the option to add the LINE Official Account as a friend.   The default value is none. 
         class LiffBotPrompt
-
-          def initialize(
-            **dynamic_attributes
-          )
-            
-
-            dynamic_attributes.each do |key, value|
-              self.class.attr_accessor key
-
-              if value.is_a?(Hash)
-                struct_klass = Struct.new(*value.keys.map(&:to_sym))
-                struct_values = value.map { |_k, v| v.is_a?(Hash) ? Line::Bot::V2::Utils.hash_to_struct(v) : v }
-                instance_variable_set("@#{key}", struct_klass.new(*struct_values))
-              else
-                instance_variable_set("@#{key}", value)
-              end
-            end
-          end
-
-          def self.create(args) # steep:ignore
-            return new(**args) # steep:ignore
-          end
-
-          # @param other [Object] Object to compare
-          # @return [Boolean] true if the objects are equal, false otherwise
-          def ==(other)
-            return false unless self.class == other.class
-
-            instance_variables.all? do |var|
-                instance_variable_get(var) == other.instance_variable_get(var)
-            end
-          end
-
-          # @return [Integer] Hash code of the object
-          def hash
-            [self.class, *instance_variables.map { |var| instance_variable_get(var) }].hash
-          end
+          
         end
       end
     end
