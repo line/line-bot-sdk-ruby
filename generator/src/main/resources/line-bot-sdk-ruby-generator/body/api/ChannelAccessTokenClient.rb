5c2e49c3ffcdaf960031ac4e3abfd033f29a4e8e
habara keigo <habara.keigo@lycorp.co.jp>
2026-04-02 19:34:18 +0900

    Support *_with_http_info

--- generator/src/main/resources/line-bot-sdk-ruby-generator/body/api/ChannelAccessTokenClient.rb
+++ generator/src/main/resources/line-bot-sdk-ruby-generator/body/api/ChannelAccessTokenClient.rb
@@ -29,3 +29,37 @@
               client_secret: client_secret
             )
           end
+
+          # Issues a new stateless channel access token by JWT assertion.
+          # This is a convenience wrapper that only requires the parameters needed for JWT assertion authentication.
+          # When you want to get HTTP status code or response headers, use this instead of {{ '{#' }}issue_stateless_channel_token_by_jwt_assertion}.
+          #
+          # @param client_assertion [String] A JSON Web Token the client needs to create and sign with the private key of the Assertion Signing Key.
+          # @return [Array(Line::Bot::V2::ChannelAccessToken::IssueStatelessChannelAccessTokenResponse, Integer, Hash{String => String})]
+          def issue_stateless_channel_token_by_jwt_assertion_with_http_info(
+            client_assertion:
+          )
+            issue_stateless_channel_token_with_http_info(
+              grant_type: 'client_credentials',
+              client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+              client_assertion: client_assertion
+            )
+          end
+
+          # Issues a new stateless channel access token by client secret.
+          # This is a convenience wrapper that only requires the parameters needed for client secret authentication.
+          # When you want to get HTTP status code or response headers, use this instead of {{ '{#' }}issue_stateless_channel_token_by_client_secret}.
+          #
+          # @param client_id [String] Channel ID.
+          # @param client_secret [String] Channel secret.
+          # @return [Array(Line::Bot::V2::ChannelAccessToken::IssueStatelessChannelAccessTokenResponse, Integer, Hash{String => String})]
+          def issue_stateless_channel_token_by_client_secret_with_http_info(
+            client_id:,
+            client_secret:
+          )
+            issue_stateless_channel_token_with_http_info(
+              grant_type: 'client_credentials',
+              client_id: client_id,
+              client_secret: client_secret
+            )
+          end
